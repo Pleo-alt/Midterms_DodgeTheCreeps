a37246928cc792da1309d777760ba127ed339878
PleoNexia <ian13garrido@gmail.com>
2025-05-31 20:00:00 +0800

    initial commit

--- hud.gd
+++ hud.gd
@@ -2,12 +2,12 @@ extends CanvasLayer
 
 signal start_game
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	pass
 
@@ -22,13 +22,13 @@ func show_message(text):
 func show_game_over():
 	show_message("Game Over")
 	
-	# Wait until the MessageTimer has counted down.
+
 	await $MessageTimer.timeout
 
 	$Message.text = "Dodge the\nCreeps!"
 	$Message.show()
 	
-	# Make a one-shot timer and wait for it to finish.
+
 	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
 
--- player.gd
+++ player.gd
@@ -1,22 +1,22 @@
 extends Area2D
 signal hit
 
-@export var speed = 400 # How fast the player will movie (pixels/sec)
-var screen_size #Size of the game window
+@export var speed = 400 
+var screen_size 
 
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector
+	var velocity = Vector2.ZERO 
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -45,7 +45,7 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	hide() # Player disappears after being hit
+	hide() 
 	hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true) # 
 	
